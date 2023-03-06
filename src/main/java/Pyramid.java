abb00545b307e4fc0c82935d789076cad80399bd
nepalialberta <nepalialberta@gmail.com>
2023-03-06 18:32:56 +0000

    Done

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,19 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+     String s = " ";
+        for(int i = 0;i<=n;i++){
+            for(int j = 0;j<i;j++){
+            s = s + "*";
+        }
+            s = s + "\n";
+        
     }
-}
+        return s ;
+    }
+
+
+   
+
+    }
+
